48c62529d953e8614cb64cc9de1c2560b439383b
Any97Cris <criscianysilva1997@gmail.com>
2023-02-21 08:36:55 -0400

    ajute sintaxe

--- autoload.php
+++ autoload.php
@@ -5,15 +5,16 @@
 * @param $classe
  */
 
-function outoload($classe){
+function autoload($classe){
     $diretorioBase = DIR_APP . DS;
     $classe = $diretorioBase . 'Classes' . DS . str_replace('\\', DS, $classe) . '.php';
+    
 
     if(file_exists($classe) && !is_dir($classe)){
         include $classe;
     }
 }
 
-spl_autoload_register( autoload_function: 'autoload' );
+spl_autoload_register('autoload');
 
 ?>
