5b0d483193af9acca30e5d61204f683eb9c9692b
Any97Cris <criscianysilva1997@gmail.com>
2023-02-22 09:18:47 -0400

    caminho projeto

--- bootstrap.php
+++ bootstrap.php
@@ -12,6 +12,7 @@ define("SENHA", '');
 
 define("DS", DIRECTORY_SEPARATOR);
 define("DIR_APP", __DIR__);
+define("DIR_PROJETO", 'api');
 
 if(file_exists('autoload.php')){
     include 'autoload.php';
