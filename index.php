db4bb433fee82aa7f78cb7615b5f93649c7f0555
Any97Cris <criscianysilva1997@gmail.com>
2023-02-22 09:17:42 -0400

    comunicacao com banco

--- index.php
+++ index.php
@@ -3,6 +3,16 @@
 include 'bootstrap.php';
 
 use Validator\RequestValidator;
-$request = new RequestValidator();
+use Util\RotasUtil;
+
+
+try{
+    $request = new RequestValidator(RotasUtil::getRotas());
+    $retorno = $request->processarRequest();
+}catch(Exception $e){
+    echo "Error: ".$e->getMessage();
+}
+
+
 
 ?>
