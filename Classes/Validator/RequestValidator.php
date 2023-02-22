e71bbdfac701ea310fb433ef63a8d955ea635b1c
Any97Cris <criscianysilva1997@gmail.com>
2023-02-22 09:16:54 -0400

    class validador

--- Classes/Validator/RequestValidator.php
+++ Classes/Validator/RequestValidator.php
@@ -2,10 +2,38 @@
 
 namespace Validator;
 
-class RequestValidator{
+use Util\ConstantesGenericasUtil;
+use Repository\TokensAutorizadosRepository;
 
-    public function __construct(){
-        echo 'Testando autoload';
+class RequestValidator{
+    private $request;
+    private array $dadosRequest;
+    private object $tokenAutorizadosRepository;
+    const GET = 'GET';
+    const DELETE = 'DELETE';
+
+    public function __construct($request){     
+        $this->request = $request;
+        $this->tokenAutorizadosRepository = new TokensAutorizadosRepository();
+    }
+
+    public function processarRequest(){
+        $retorno = utf8_encode(ConstantesGenericasUtil::MSG_ERRO_TIPO_ROTA);
+
+        $this->request['metodo'] == 'POST';
+
+        if(in_array($this->request['metodo'], ConstantesGenericasUtil::TIPO_REQUEST, true)){
+            $retorno = $this->direcionarRequest();
+        }
+        return $retorno;
+    }
+
+    private function direcionarRequest(){
+
+        if($this->request['metodo'] !== self::GET && $this->request['metodo'] !== self::DELETE){
+            $this->dadosRequest = JsonUtil::tratarCorpoRequisicaoJson();
+        }
+
     }
 
 }
