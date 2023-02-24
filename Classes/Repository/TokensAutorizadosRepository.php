c905663674bee1c01a94a7b10b60ce05a0f0b17b
Any97Cris <criscianysilva1997@gmail.com>
2023-02-24 09:09:32 -0400

    configurações

--- Classes/Repository/TokensAutorizadosRepository.php
+++ Classes/Repository/TokensAutorizadosRepository.php
@@ -3,6 +3,7 @@
 namespace Repository;
 use DB\MySQL;
 use Util\ConstantesGenericasUtil;
+use InvalidArgumentException;
 
 class TokensAutorizadosRepository{
 
@@ -14,8 +15,23 @@ class TokensAutorizadosRepository{
     }
 
     public function validarToken($token){
-        var_dump($token);
-        exit;
+        $token = str_replace([' ', 'Bearer'], '', $token);
+        
+        if($token){
+            $consultaToken = 'SELECT id FROM ' . self::TABELA . ' WHERE token = :token AND status = :status';
+            $stmt = $this->getMySQL()->getDb()->prepare($consultaToken);
+            $stmt->bindValue(":token", $token);
+            $stmt->bindValue(":status", ConstantesGenericasUtil::SIM);
+            $stmt->execute();
+
+            if($stmt->rowCount() !== 1){
+                header('HTTP/1.1 401 Unauthorized');
+                throw new \InvalidArgumentException(ConstantesGenericasUtil::MSG_ERRO_TOKEN_NAO_AUTORIZADO);
+            }
+
+        }else{
+            throw new \InvalidArgumentException(ConstantesGenericasUtil::MSG_ERRO_TOKEN_VAZIO);
+        }
     }
 
     public function getMySQL(){
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 use Util\RotasUtil;
 use Validator\RequestValidator;
+use Util\ConstantesGenericasUtil;
 
 include 'bootstrap.php';
 
@@ -9,7 +10,12 @@ try{
     $request = new RequestValidator(RotasUtil::getRotas());
     $retorno = $request->processarRequest();
 }catch(Exception $e){
-    echo "Error: ".$e->getMessage();
+    echo json_encode([
+        ConstantesGenericasUtil::TIPO => ConstantesGenericasUtil::TIPO_ERRO,
+        ConstantesGenericasUtil::RESPOSTA => utf8_encode($e->getMessage())
+
+    ]);
+    exit;
 }
 
 
